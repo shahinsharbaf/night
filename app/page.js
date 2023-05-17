2b034325670794074230b22e8230617205c4b30c
shahinsharbaf <88193227+shahinsharbaf@users.noreply.github.com>
2023-05-17 23:32:49 +0330

    fix

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>hello</h1>;
+  return (
+    <Image src="/r.jpg" alt="Vercel Logo" width={500} height={240} priority />
+  );
 }
