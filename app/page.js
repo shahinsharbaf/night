6f14c52668dc1eee76427e6cae7a48ef5815f658
shahinsharbaf <88193227+shahinsharbaf@users.noreply.github.com>
2023-05-18 09:53:28 +0330

    sexy

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
 import Image from "next/image";
 import img2 from "/public/image/bb.jpg";
+import img3 from "/public/image/b2.jpg";
 
 export default function Home() {
   return (
-    <Image src={img2} alt="Vercel Logo" width={500} height={340} priority />
+    <>
+      <Image src={img2} alt="Vercel Logo" width={500} height={340} priority />
+      <Image src={img3} alt="Vercel Logo" width={500} height={340} priority />
+    </>
   );
 }
