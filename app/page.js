7eef8e93a84aafe9cca64403b8239309fe8cae85
shahinsharbaf <88193227+shahinsharbaf@users.noreply.github.com>
2023-05-18 23:25:28 +0330

    ppo

--- app/page.js
+++ app/page.js
@@ -1,14 +1,8 @@
-import { sql } from "@vercel/postgres";
-
-const likes = 100;
-const { rows } = await sql`SELECT * FROM posts WHERE likes > ${likes};`;
-console.log(rows);
 export default async function Home() {
   return (
     <>
       <div className="flex">
         <h1>salam</h1>
-        {/* <h1>Hello {userWithEmail[0].firstname}</h1> */}
       </div>
     </>
   );
