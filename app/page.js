6ae9724f6d4a30e46cded27824da59e83e2debc8
shahinsharbaf <88193227+shahinsharbaf@users.noreply.github.com>
2023-05-18 10:22:09 +0330

    last

--- app/page.js
+++ app/page.js
@@ -6,9 +6,19 @@ export default function Home() {
   return (
     <>
       <div className="flex">
-        <Image src={img2} alt="Vercel Logo" width={500} height={340} priority />
-        <Image src={img3} alt="Vercel Logo" width={500} height={340} priority />
+        <Image src={img2} alt="Vercel Logo" width={200} height={140} priority />
+        <Image src={img3} alt="Vercel Logo" width={200} height={140} priority />
+        {/* <video autoPlay loop style={{ width: "500px", height: "500px" }}>
+          <source src="/blue.mp4" />
+        </video> */}
       </div>
+      <iframe
+        src="https://www.youtube.com/embed/E7wJTI-1dvQ"
+        frameborder="0"
+        allow="autoplay; encrypted-media"
+        allowfullscreen
+        title="video"
+      />
     </>
   );
 }
